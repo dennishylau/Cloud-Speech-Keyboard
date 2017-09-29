5fa5ab36f46b44f595a119d5ebb56cdbe819e269
Dennis Lau <dennishylau@gmail.com>
2017-09-29 18:14:59 +0800

    Initial Commit

---
+++ Cloud Speech Keyboard/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Cloud Speech Keyboard
+//
+//  Created by Dennis Lau on 2017-09-29.
+//  Copyright © 2017 Dennis Lau. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
